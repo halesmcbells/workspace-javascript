41fdaf7eafe875ce155889865b60be723901abdc
halesmcbells <malletth@usca.edu>
2022-10-10 12:17:24 -0500

    Oct10_2022

---
+++ js/arith.js
@@ -0,0 +1,14 @@
+//Cool external javascript file
+//Get Elements-- Get the text
+let spanE = document.getElementById('num1');
+let spanT = spanE.textContent; //assign element but now its a string
+//convert to int
+let n1 = parseInt(spanT);
+
+let n2 = parseInt(document.getElementById('num2').textContent);
+
+//add the numbers
+let c = n1 + n2;
+
+//display
+document.write(c);
